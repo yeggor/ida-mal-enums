3c05c59aad80456fa6ff543631b3825b5e4a0601
yeggor <vasilenko.yegor@gmail.com>
2020-10-28 01:56:38 +0300

    new hashes

--- api_hashes_enums.py
+++ api_hashes_enums.py
@@ -11,7 +11,7 @@ HASHES_TMPL = os.path.join(CURRENT_DIR, 'shellcode_hashes', 'hashes', '*')
 
 # uncomment the hash type if you want to create enumerations from it
 HASH_TYPES = [
-    'crc32',
+    # 'crc32',
     # 'ror11AddHash32',
     # 'rol7XorHash32',
     # 'hash_Carbanak',
@@ -34,6 +34,8 @@ HASH_TYPES = [
     # 'fnv1Xor67f',
     # 'addRor4WithNullHash32',
     # 'dualaccModFFF1Hash'
+    # 'blockhash',
+    'pjw_hash'
 ]
 
 
@@ -45,29 +47,32 @@ def create_enums(lib_fpath):
         enum_name = '{}_{}'.format(lib_name, hash_name)
         enum_ids[enum_name] = idc.add_enum(-1, enum_name, ida_bytes.dec_flag())
     f = open(lib_fpath, 'r')
-    while(True):
+    while (True):
         line_csv = f.readline()
         if not line_csv:
             break
-        csv_hash_value, csv_func_name, csv_lib_name, csv_hash_name = line_csv.split(',')
+        csv_hash_value, csv_func_name, csv_lib_name, csv_hash_name = line_csv.split(
+            ',')
         csv_hash_name = csv_hash_name[:len(csv_hash_name) - 1]
         if not csv_hash_name in HASH_TYPES:
             continue
         csv_lib_name, _ = os.path.splitext(csv_lib_name)
         csv_enum_name = '{}_{}'.format(csv_lib_name, csv_hash_name)
-        idc.add_enum_member(
-            enum_ids[csv_enum_name],
-            '{}_{}'.format(csv_hash_name, csv_func_name),
-            int(csv_hash_value, 16),
-            -1
-        )
+        idc.add_enum_member(enum_ids[csv_enum_name],
+                            '{}_{}'.format(csv_hash_name, csv_func_name),
+                            int(csv_hash_value, 16), -1)
     f.close()
+
 
 def main():
     libs = glob.glob(HASHES_TMPL)
     for lib in libs:
         print('[current file] {}'.format(lib))
-        create_enums(lib)
+        try:
+            create_enums(lib)
+        except Exception as e:
+            print('[error] {}'.format(repr(e)))
+
 
 if __name__ == '__main__':
     main()
